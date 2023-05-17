da06baa58bb2dd7b4365f7512958a1dfb50e1777
SmitasCodes <airidassmith30@gmail.com>
2023-05-17 23:29:29 +0300

    first commit signin signup added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SingUp";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
