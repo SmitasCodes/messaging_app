cec2ad60dd3f5dc7d83ac8d5c6aaaec47130ff32
SmitasCodes <airidassmith30@gmail.com>
2023-06-01 13:25:02 +0300

    updated code

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,10 +14,4 @@ const firebaseConfig = {
   appId: "1:850782169052:web:209a603ae4dc7d2f109119",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-export default db;
+export default firebaseConfig;
